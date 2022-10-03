fd30e3155117489a1b186b4f378fefcf21ff1e23
Gilang Al Khansa <ext.gilang.khansa@grabtaxi.com>
2022-10-03 23:49:24 +0700

    fix unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
 test('renders learn react link', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+  render(<App />, { wrapper: BrowserRouter })
+  const appClassName = screen.getByText(/.Github Users/i)
+  expect(appClassName).toBeInTheDocument()
 })
